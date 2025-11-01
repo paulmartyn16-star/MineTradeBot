2089daa1b3cb0f4269ce987d646cc95a84cfff5d
paulmartyn16-star <deine_github_email@beispiel.com>
2025-11-01 12:18:49 +0100

    s

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ passport.use(
     {
       clientID: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
-      callbackURL: "https://V0.onrender.com/callback",
+      callbackURL: "https://v0.onrender.com/callback",
       scope: ["identify", "guilds", "guilds.members.read"],
     },
     (accessToken, refreshToken, profile, done) => done(null, profile)
